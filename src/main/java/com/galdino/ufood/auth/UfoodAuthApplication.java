f4dabc55e94c8936cb8809d1b0db40e5db7bed0e
Fernando Galdino <fernando.galdino@meta.com.br>
2025-04-14 18:39:23 -0300

    first commit

---
+++ src/main/java/com/galdino/ufood/auth/UfoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.galdino.ufood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfoodAuthApplication.class, args);
+	}
+
+}
