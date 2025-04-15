c1e7afde0661ffbe5266bdc060d71071765fabab
Fernando Galdino <fgaldinoo@gmail.com>
2025-04-15 18:54:18 -0300

    add Spring Security OAuth2 dependency and WebSecurityConfig class

---
+++ src/main/java/com/galdino/ufood/auth/AuthorizationServerConfig.java
@@ -0,0 +1,10 @@
+package com.galdino.ufood.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+}
---
+++ src/main/java/com/galdino/ufood/auth/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.galdino.ufood.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("ufood")
+                .password(passwordEncoder().encode("123"))
+                .roles("admin")
+            .and()
+            .withUser("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("admin");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
