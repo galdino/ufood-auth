3a34bd41620d08aa15322e106863e15e7cfa2e3a
Fernando Galdino <fgaldinoo@gmail.com>
2025-04-24 18:49:52 -0300

    configure new client credentials to verify tokens

--- src/main/java/com/galdino/ufood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/galdino/ufood/auth/AuthorizationServerConfig.java
@@ -24,11 +24,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("ufood-web")
-                .secret(passwordEncoder.encode("web123"))
-                .authorizedGrantTypes("password")
-                .scopes("write", "read")
-                .accessTokenValiditySeconds(60 * 60 * 6);
+                    .withClient("ufood-web")
+                    .secret(passwordEncoder.encode("web123"))
+                    .authorizedGrantTypes("password")
+                    .scopes("write", "read")
+                    .accessTokenValiditySeconds(60 * 60 * 6)
+               .and()
+                    .withClient("checktoken")
+                    .secret(passwordEncoder.encode("check123"));
     }
 
     @Override
